e99b1fb711ff5457723f7a717cf4874a002dea4f
Tobias <qenzoz@gmail.com>
2024-09-27 00:34:38 +0200

    Referer til SharedTestDependencies

--- server/tests/ApiIntegrationTests/OrderTest.cs
+++ server/tests/ApiIntegrationTests/OrderTest.cs
@@ -35,11 +35,11 @@ public class OrderTest : WebApplicationFactory<Program>
         Assert.NotEqual(0, responseData.Id);
         Assert.True(responseData.TotalPrice > 0);
 
-        Assert.NotNull(responseData.Orders);
-        Assert.NotEmpty(responseData.Orders);
+        Assert.NotNull(responseData.Entry);
+        Assert.NotEmpty(responseData.Entry);
 
         var entry = createOrderModel.OrderEntries.First();
-        var responseEntry = responseData.Orders.First();
+        var responseEntry = responseData.Entry.First();
         Assert.True(entry.ProductId == responseEntry.ProductId);
         Assert.True(entry.Quantity == responseEntry.Quantity);
     }
