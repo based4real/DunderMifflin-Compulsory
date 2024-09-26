9b0b743ec69b219c8f4b8811f9e02863a74f0bae
based4real <45218699+based4real@users.noreply.github.com>
2024-09-26 22:44:14 +0200

    Tilføjet Produkt Navn til OrderEntryDetailViewModel

--- server/Service/Models/Responses/OrderDetailViewModel.cs
+++ server/Service/Models/Responses/OrderDetailViewModel.cs
@@ -5,21 +5,12 @@ namespace Service.Models.Responses;
 public class OrderDetailViewModel
 {
     public int Id { get; set; }
+    public string? Status { get; set; } // enum?
     public DateTime OrderDate { get; set; }
     public DateOnly? DeliveryDate { get; set; }
-    public string? Status { get; set; } // enum?
     public double TotalPrice { get; set; }
     
-    public required IEnumerable<Item> Entry { get; set; }
-
-    public class Item
-    {
-        public int Id { get; set; }
-        public int Quantity { get; set; }
-        public int? ProductId { get; set; }
-        public double Price { get; set; }
-        public double TotalPrice { get; set; }
-    }
+    public required IEnumerable<OrderEntryDetailViewModel> Entry { get; set; }
 
     public static OrderDetailViewModel FromEntity(Order order)
     {
@@ -30,14 +21,7 @@ public class OrderDetailViewModel
             DeliveryDate = order.DeliveryDate,
             Status = order.Status,
             TotalPrice = order.TotalAmount,
-            Entry = order.OrderEntries.Select(x => new Item
-            {
-                Id = x.Id,
-                Quantity = x.Quantity,
-                ProductId = x.ProductId,
-                Price = x.Product?.Price ?? 0,
-                TotalPrice = x.Product?.Price * x.Quantity ?? 0
-            })
+            Entry = order.OrderEntries.Select(x => OrderEntryDetailViewModel.fromEntity(x))
         };
     }
 }
---
+++ server/Service/Models/Responses/OrderEntryDetailViewModel.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.Models;
+
+namespace Service.Models.Responses;
+
+public class OrderEntryDetailViewModel
+{
+    public int Id { get; set; }
+    public int? ProductId { get; set; }
+    public string? ProductName { get; set; }
+    public int Quantity { get; set; }
+    public double Price { get; set; }
+    public double TotalPrice { get; set; }
+
+    public static OrderEntryDetailViewModel fromEntity(OrderEntry entry)
+    {
+        return new OrderEntryDetailViewModel
+        {
+            Id = entry.Id,
+            ProductId = entry.ProductId,
+            ProductName = entry.Product?.Name ?? "Not available",
+            Price = entry.Product?.Price ?? 0,
+            Quantity = entry.Quantity,
+            TotalPrice = entry.Product?.Price * entry.Quantity ?? 0
+        };
+    }
+}
