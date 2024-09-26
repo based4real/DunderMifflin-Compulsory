162eedbeeb285543c988ff815197b12d9bf8fd52
Tobias <qenzoz@gmail.com>
2024-09-27 00:08:42 +0200

    Tilføj DataAnnotation til AppOptions

--- server/Service/AppOptions.cs
+++ server/Service/AppOptions.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Service;
 
 public class AppOptions
 {
+    [Required(ErrorMessage = "LocalDbConn is required.")]
+    [MinLength(1, ErrorMessage = "LocalDbConn cannot be an empty string.")]
     public string LocalDbConn { get; set; } = null!;
 }
