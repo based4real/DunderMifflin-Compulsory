81eae8a2409460c2a186b560e14c561695226d0a
based4real <45218699+based4real@users.noreply.github.com>
2024-09-27 20:02:55 +0200

    Tilføjet controller metode kigger op mod serivce, samt dokumentation på API

--- server/API/Controllers/PaperController.cs
+++ server/API/Controllers/PaperController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Service.Interfaces;
+using Service.Models.Requests;
+using Service.Models.Responses;
 
 namespace API.Controllers;
 
@@ -7,5 +9,19 @@ namespace API.Controllers;
 [Route("api/[controller]")]
 public class PaperController(IPaperService service) : ControllerBase
 {
-    
+    /// <summary>
+    /// Creates a Paper Property.
+    /// </summary>
+    /// <param name="property">The property details</param>
+    /// <returns>The created property.</returns>
+    /// <response code="201">Property created successfully.</response>
+    [HttpPost]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(PaperPropertyDetailViewModel), StatusCodes.Status201Created)]
+    public async Task<ActionResult<PaperPropertyDetailViewModel>> CreatePaperProperty([FromBody] PaperPropertyCreateModel property)
+    {
+        var createdProperty = await service.CreateProperty(property);
+        return CreatedAtAction(nameof(CreatePaperProperty), new { id = createdProperty.Id }, createdProperty);
+    }
 }
