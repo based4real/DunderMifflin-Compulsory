1757f16b6e93ff2813caa15161605094363b4b4f
Tobias <qenzoz@gmail.com>
2024-09-27 23:12:40 +0200

    Tilføj powershell script for at starte backend

---
+++ run_backend.ps1
@@ -0,0 +1,8 @@
+﻿$composeStatus = docker-compose ps --services --filter "status=running"
+
+if (-not $composeStatus) {
+    Write-Output "Docker services are not running. Starting Docker Compose..."
+    docker-compose up -d
+}
+
+dotnet run --project ./server/api
