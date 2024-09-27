8e19faf79855b3d56acc6dcdbe6028f84f0e9821
Tobias <qenzoz@gmail.com>
2024-09-27 19:30:13 +0200

    Få TotalPages med i PagingInfo

--- server/Service/Models/Responses/CustomerOrderPagedViewModel.cs
+++ server/Service/Models/Responses/CustomerOrderPagedViewModel.cs
@@ -1,18 +1,7 @@
-﻿using DataAccess.Models;
-
-namespace Service.Models.Responses;
+﻿namespace Service.Models.Responses;
 
 public class CustomerOrderPagedViewModel
 {
     public CustomerOrderDetailViewModel CustomerDetails { get; set; } = null!;
     public PagingInfo PagingInfo { get; set; } = new();
-    
-    public static CustomerOrderPagedViewModel FromEntity(Customer customer, PagingInfo pagingInfo)
-    {
-        return new CustomerOrderPagedViewModel
-        {
-            CustomerDetails = CustomerOrderDetailViewModel.FromEntity(customer),
-            PagingInfo = pagingInfo
-        };
-    }
 }
