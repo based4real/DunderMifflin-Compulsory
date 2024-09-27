cc903f679d84bab82abf91fcca3cbc6c6e797b6c
Tobias <qenzoz@gmail.com>
2024-09-27 19:30:13 +0200

    Få TotalPages med i PagingInfo

--- server/Service/Models/PagingInfo.cs
+++ server/Service/Models/PagingInfo.cs
@@ -1,10 +1,15 @@
-﻿namespace Service.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace Service.Models;
 
 public class PagingInfo
 {
    public int TotalItems { get; set; }
    public int ItemsPerPage { get; set; }
    public int CurrentPage { get; set; }
+
+   [JsonPropertyName("totalPages")] // Gør sådan at totalPages kommer med i JSON respons
+   public int TotalPagesForSerialization => TotalPages;
    
-   public int TotalPages() => (int) Math.Ceiling((decimal) TotalItems / ItemsPerPage);
+   private int TotalPages => (int) Math.Ceiling((decimal) TotalItems / ItemsPerPage);
 }
